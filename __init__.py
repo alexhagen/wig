6b5b1c36bcc7e7862ff2f87ef29a139da4fd898f
Alex Hagen <ahagen@purdue.edu>
2017-12-20 09:05:10 -0500

    got the import machinery working. and updated the gitignore for checkpoints

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .geo import geo as g
+from .matl import matl as m
+from .cell import cell as c
+from .phys import phys as p
+from .source import source as s
+from .tally import tally as t
+from .analyze import analyze as a
